b7ad846cff6e08d276adea53f4d0c1563ffa2a11
hamed-yazdi <32354955+hamed-yazdi@users.noreply.github.com>
2022-12-21 23:36:43 +0100

    Update benchmark.c

--- benchmark.c
+++ benchmark.c
@@ -1,10 +1,10 @@
 /*
- * This program is deployed in order compare performance
+ * This program is developed in order compare performance
  * of different Linux scheduling policies such as FIFO, RR,
  * OTHER (CFS), DEADLINE, BATCH and IDLE.
- * By this way the main function run a CPU based process as multi-thread.
+ * By this way, the main function runs a CPU based process as multi-thread.
  *
- * Real Time, User Time and System Time of each thread calculate addition to
+ * The Real Time, User Time and System Time of each thread is calculated. In addition,
  * total execution time of this program calculated in order to measure such
  * interesting metrics like turnaround time, latency, fairness and efficiency
  * of each scheduling policy in Linux machines.
